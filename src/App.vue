ffb07911ae3e60f266419d100c952a1fb0e62a19
SLaks <Dev@SLaks.net>
2020-05-22 14:24:35 -0400

    Add reflect-metadata

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-    </div>
+  <div id="app">
+    <Home />
+  </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import Home from './components/Home.vue';
+import "reflect-metadata";
+import { Component, Vue } from "vue-property-decorator";
+import Home from "./components/Home.vue";
 
-    @Component({
-        components: {
-            Home
-        }
-    })
-    export default class App extends Vue {
-
-    }
+@Component({
+  components: {
+    Home
+  }
+})
+export default class App extends Vue {}
 </script>
 
 <style>
-    #app {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    }
+#app {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
 </style>
