b5be684158f728002e01d0fc2a813698d01e96b0
SLaks <Dev@SLaks.net>
2020-05-25 22:32:05 -0400

    Display event periods

--- src/components/EventDefinitionUI.vue
+++ src/components/EventDefinitionUI.vue
@@ -9,7 +9,7 @@
     <td style="width: 144px;">
       <DatePicker dense v-model="def.startDate" />
     </td>
-    <td>Recur?</td>
+    <td>{{periodDescription}} {{untilDescription}}</td>
     <td>
       <v-btn icon small @click="$emit('delete')" title="Delete event">
         <v-icon small>mdi-delete</v-icon>
@@ -24,6 +24,8 @@ import CurrencyField from "./widgets/CurrencyField.vue";
 import DatePicker from "./widgets/DatePicker.vue";
 import { EventDefinition } from "@/schema";
 import equal from "fast-deep-equal";
+import ordinal from "ordinal";
+import { format } from "date-fns";
 
 @Component({
   components: { DatePicker, CurrencyField }
@@ -32,6 +34,39 @@ export default class EventDefinitionUI extends Vue {
   @Prop() value!: EventDefinition;
 
   def = { ...this.value };
+  readonly weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Saturday"
+  ];
+
+  get periodDescription() {
+    switch (this.def.period) {
+      case "months":
+        let str;
+        if (this.def.periodCount === 1) str = "Every month";
+        else str = `Every ${this.def.periodCount} months`;
+        return `${str} on the ${ordinal(this.def.periodSubIndex + 1)}`;
+
+      case "weeks":
+        const day = this.weekdays[this.def.periodSubIndex];
+        if (this.def.periodCount === 1) return `Every ${day}`;
+        else return `Every ${this.def.periodCount} ${day}s`;
+    }
+  }
+
+  get untilDescription() {
+    if (!this.def.endDate) return null;
+    return `Until ${format(this.def.endDate, "MM/dd/yyyy")}`;
+  }
+
+  get pluralizedPeriod() {
+    if (this.def.periodCount === 1) return this.def.period.replace(/s$/, "");
+    return this.def.period;
+  }
 
   hover = false;
 
